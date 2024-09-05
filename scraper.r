cc62a96257c8bb1ce9adb5d5e2c10000a9ba8a94
Qlawson3 <165700036+Qlawson3@users.noreply.github.com>
2024-09-05 16:39:37 -0400

    Update scraper.r

--- scraper.r
+++ scraper.r
@@ -1,5 +1,5 @@
-install.packages("rvest")
-install.packages("tidyverse")
+library(rvest)
+library(tidyverse)
 
 #Other scaper is called scraper.py
 #For MSISAC parse through advisory page
@@ -8,35 +8,47 @@ install.packages("tidyverse")
 
 url <- "https://www.cisecurity.org/advisory"
 
-#Equivalent of response.content (I believe)
-CIShtml <- read.html(url)
+#You may want to start looping through pages from here
+#Equivalent of response.content
+CIShtml <- read_html(url)
 
 #Title Parser
 #Equivalent of .find_all and .get text in one line
-title = html_nodes(CIShtml, .c-list-link) %>% html_text()
-
+title = html_nodes(CIShtml, ".c-list-link") %>% html_text()
+print(title)
 #CVE Parser
 
 for(a_tag in title){
   #gather href links for each advisory on each page
-  link = title %>% html_attr("href")
-  linked_url <- file.path(url, link)
-
-  #parse through connected page
-  CIShtml_2 <- read.html(linked_url)
-
-  a_tags_linked = html_nodes(CIShtml_2, .reference-list div) %>% html_text()
-  #find all linked pages with CVE in title of a tag
-
-  #grepl is equivalent of x in y or contains
-  if (grepl("CVE", a_tags_linked)){
-    #get only substring of CVE (possibly use gregexpr)
-    }
+  link = html_nodes(CIShtml, ".c-list-link") %>% html_attr("href")
+  linked_url <- paste0("https://www.cisecurity.org", link)
+  
+  
+  for(link in linked_url){
+    file <- file.path(link)
+    #parse through each connected page
+    CIShtml_2 <- read_html(file)
+  
+    a_tags_linked = html_nodes(CIShtml_2, ".reference-list div") %>% html_text()
+    
+    #find all linked pages with CVE in title of a tag
+    for(a_tag_linked in a_tags_linked){
+      #grepl is equivalent of x in y or contains
+      if (grepl("CVE", a_tag_linked)){
+      #get only substring of CVE (possibly use gregexpr)
+      cvematch <- gregexpr("CVE-\\d+-\\d+", a_tags_linked)
+      result <- regmatches(a_tags_linked, cvematch)
+      print(result)
+      } else{
+        print("No CVE Found")
+      }
+      
   }
-
-
+}
+}
 #Date Parser
-date = html_nodes(CIShtml, .c-list-date) %>% html_text()
+date = html_nodes(CIShtml, ".c-list-date") %>% html_text()
+print(date)
 
 #After fixing the above, the scrapper must loop through every page, save information to a table (possibly use data frame) and
 #export to excel
